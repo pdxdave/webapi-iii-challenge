4f1ba8e63630196751286f1ef29130e59f24e54c
kamaboko1 <github65@hotmail.com>
2019-06-05 11:46:02 -0700

    setting up postRouter and server

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,37 @@
-const express = 'express';
+// bring in express
+const express = require('express');
 
+// create express router
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// link to posts
+const Posts = require('./postDb');
 
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get(req.query)
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
 });
 
 router.get('/:id', (req, res) => {
-
+      try {
+          const post = await Posts.findById(req.params.id)
+          if (post) {
+              res.status(200).json(post)
+          } else {
+              res.status(404).json({ message: "Post not found"})
+          }
+      } catch (error) {
+          res.status(500).json({
+              message: "Error retrieving the post"
+          })
+      }
 });
 
 router.delete('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = 'express';
+// bring in express
+const express = require('express');
 
+// create express server
 const server = express();
+
+// middleware
+server.use(logger)
+
+// require router
+const PostsRouter = require('./posts/postRouter')
+
+server.use('/api/posts', PostsRouter)
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +20,7 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${req.method} Request`)
 };
 
 module.exports = server;
